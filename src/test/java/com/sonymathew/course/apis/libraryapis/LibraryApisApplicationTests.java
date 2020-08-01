b246a777f3439977f18642b56c9a28fe8be0ada6
Sony Mathew <mailsonymathew@gmail.com>
2020-08-01 13:13:32 +0530

    Author REST API working complete

---
+++ src/test/java/com/sonymathew/course/apis/libraryapis/LibraryApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonymathew.course.apis.libraryapis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/sonymathew/course/apis/libraryapis/testutils/LibraryApiTestUtil.java
@@ -0,0 +1,43 @@
+package com.sonymathew.course.apis.libraryapis.testutils;
+
+import com.sonymathew.course.apis.libraryapis.author.Author;
+import com.sonymathew.course.apis.libraryapis.author.AuthorEntity;
+import com.sonymathew.course.apis.libraryapis.model.common.Gender;
+import com.sonymathew.course.apis.libraryapis.publisher.Publisher;
+import com.sonymathew.course.apis.libraryapis.publisher.PublisherEntity;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public class LibraryApiTestUtil {
+
+    public static Publisher createPublisher() {
+        return new Publisher(null, TestConstants.TEST_PUBLISHER_NAME,
+                TestConstants.TEST_PUBLISHER_EMAIL,
+                TestConstants.TEST_PUBLISHER_PHONE);
+    }
+
+    public static PublisherEntity createPublisherEntity() {
+        return new PublisherEntity(TestConstants.TEST_PUBLISHER_NAME,
+                TestConstants.TEST_PUBLISHER_EMAIL,
+                TestConstants.TEST_PUBLISHER_PHONE);
+    }
+
+    public static Optional<PublisherEntity> createPublisherEntityOptional() {
+        return Optional.of(createPublisherEntity());
+    }
+
+    public static Author createAuthor() {
+        return new Author(null, TestConstants.TEST_AUTHOR_FIRST_NAME,
+                TestConstants.TEST_AUTHOR_LAST_NAME, LocalDate.now().minusYears(30), Gender.Female);
+    }
+
+    public static AuthorEntity createAuthorEntity() {
+        return new AuthorEntity(TestConstants.TEST_AUTHOR_FIRST_NAME,
+                TestConstants.TEST_AUTHOR_LAST_NAME, LocalDate.now().minusYears(30), Gender.Female);
+    }
+
+    public static Optional<AuthorEntity> createAuthorEntityOptional() {
+        return Optional.of(createAuthorEntity());
+    }
+}
